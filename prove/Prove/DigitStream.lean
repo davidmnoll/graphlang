adf9e82712d1b3d9d1e39ec04601afe050a611b5
dmn <dmn322@gmail.com>
2025-07-15 00:22:37 -0700

    wip

--- prove/Prove/DigitStream.lean
+++ prove/Prove/DigitStream.lean
@@ -1,12 +1,86 @@
 import Mathlib.Data.Complex.Basic
-
-def z : ℂ := ⟨3, 4⟩
-
-
-
-structure NatBit where :=
+import Mathlib.Data.Stream.Basic
+import Std.Data.List.Basic
 
 
 
-structure StreamGaussianInt where
-def streamFromComplex :=
+inductive BitBody
+  | zeros   -- ∞ stream of 0s
+  | ones    -- ∞ stream of 1s
+  | stream (s : Stream Bit)
+  deriving Repr
+
+
+
+def CStream := ℂ → Bit
+
+def getCStream : ℂ → CStream := fun (re, im) =>
+  let front RealBitStrWithBody BitBody.ones := {
+    head := [],
+    body := BitBody.ones
+    tail := []
+  }
+  let ComplexBitStr
+
+
+structure ComplexBitStrSum where :=
+  operand: List ComplexBitStr
+
+structure ComplexBitStrProd where :=
+  entries: List ComplexBitStr
+
+structure ComplexBitStrExp where :=
+  entry: List ComplexBitStr
+
+
+def zeroCStream : CStream := fun _ => false
+
+instance : Zero CStream where
+  zero := zeroCStream
+
+
+instance : Add CStream where
+  add := addCStream
+
+instance : AddCommGroup CStream where
+  add := (· + ·)
+  zero := 0
+  neg := id
+  nsmul := fun n s => if n % 2 = 0 then 0 else s
+  zsmul := fun z s => if z % 2 = 0 then 0 else s
+
+instance : Mul CStream where
+  mul := CStream.mulNoCarry  -- or your carry-aware mul
+
+def oneCStream : CStream :=
+  {
+    get := fun z => if z = { re := 0, im := 0 } then true else false
+  }
+
+instance : One CStream where
+  one := oneCStream
+
+instance : Pow CStream Nat where
+  pow := powHelper
+
+
+
+def addCStream (a b : CStream) : CStream :=
+
+
+abbrev Bit := Bool
+
+structure RealBitStr where :=
+  head : List Bit
+  body   : BitBody
+  tail : List Bit
+  deriving Repr
+
+
+structure RealBitStrWithBody (b : BitBody) where
+  val : RealBitStr
+  body_eq : val.body = b
+
+structure ComplexBitStr where :=
+  front: RealBitStrWithBody BitBody.ones
+  back: RealBitStrWithBody BitBody.zeros
--- prove/lakefile.lean
+++ prove/lakefile.lean
@@ -6,6 +6,9 @@ package prove
 require batteries from git
   "https://github.com/leanprover-community/batteries" @ "main"
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4" @ "master"
+
 @[default_target]
 lean_lib Prove
 
