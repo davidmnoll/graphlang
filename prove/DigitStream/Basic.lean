956d60140733a55a37dcde23653c3b2170180a39
dmn <dmn322@gmail.com>
2025-07-17 06:58:21 -0700

    wip

--- prove/DigitStream/Basic.lean
+++ prove/DigitStream/Basic.lean
@@ -1,38 +1,102 @@
-
+import Mathlib.Data.Nat.Basic
+import Mathlib.Data.Rat.Init
+import Mathlib.Data.Int.Basic
+import Mathlib.Algebra.Group.Basic
 
 abbrev Bit := Bool
 
 inductive BSExpr
-| const  : ℚ
-| incr_at : BSExpr
-| decr_at : BSExpr
-| add    : BSExpr → BSExpr
--- | mul    : BSExpr → BSExpr → BSExpr
-| var    : String → BSExpr
-| neg    : BSExpr
+| const  : ℚ -> BSExpr
+| nat   : Nat → BSExpr
+| incr_at : BSExpr → BSExpr → BSExpr
+-- | complex  : ℂ -> BSExpr
+-- | add    : BSExpr → BSExpr → BSExpr
+-- | mul    : BSExpr → BSExpr → BSExprnst
+-- | var    : String → BSExpr
+| flipped    : BSExpr -> BSExpr
 -- | inv    : BSExpr → BSExpr                       -- 1 / x
 -- | exp    : BSExpr → BSExpr → BSExpr              -- x^y
-| concat : BSExpr → BSExpr
+| sum : BSExpr → BSExpr → BSExpr
 -- | I  -- concat ones zeros
 | zeros
 | ones
+-- | inf
 -- | real  : ℝ
 -- | complex : ℂ
+deriving Repr
 
 
-def nsmulBSExpr  : ℕ → BSExpr → BSExpr
-| const const
+
+class BitStream (a : Type*) where
+  incr: a → BSExpr
+  flipped: a → BSExpr
+  bit_at: a -> BSExpr -> Option Bool
+
+instance : BitStream BSExpr where
+  incr e := match e with
+  | .zeros => .ones
+  | .ones => .const 1
+
+
+
+instance : BitStream Nat where
+  incr n := (.const (n + 1))
+  flipped n := .const (-(n: Int))
+  bit_at n e := match e with
+  | .zeros => false
+  | .ones => true
+  | .flipped e' => not (e.bit_at n e')
+  | .nat m => ((n / (2^m)) % 2) = 1
+  | .sum e1 e2 => e1.bit_at n e2 || e2.bit_at n e1
+  | .incr_at e1 e2 => e1.bit_at n e2 || e2.bit_at n e1
+  | .const q =>
+    if q >= 0 ∧ q.den = 1 then  -- Check if q is a non-negative integer
+      ((n / (2^q)) % 2) = 1
+    else
+      false
+
+
+
+def nsmulBSExpr : ℕ → BSExpr → BSExpr
+| 0, _ => .zeros
+| _, .zeros => .zeros
+| n, .ones => (.const (-(n: ℚ)))
+| n, .nat m => if (( n > 0 ) && (m > 0)) then .nat (n * (m: ℕ)) else .zeros
+| n, .const q => .const (q * (n: ℚ))
+| n, .concat e1 e2 => .concat (nsmulBSExpr n e1) (nsmulBSExpr n e2)
+| n, .incr_at e1 e2 => .incr_at (nsmulBSExpr n e1) (nsmulBSExpr n e2)
+| n, .flipped e => .flipped (nsmulBSExpr n e)
+-- | n, .add e1 e2 => .add (nsmulBSExpr n e1) (nsmulBSExpr n e2)
+-- | n, .mul e
+
+
+def zsmulBSExpr: Int → BSExpr → BSExpr
+| 0, _ => .zeros
+| _, .zeros => .zeros
+| z, .ones => .const (-z)
+| z, .nat n => .const (z * (n: Int))
+| z, .const q => .const (q * (z: ℚ))
+| z, .concat e1 e2 => .concat (zsmulBSExpr z e1) (zsmulBSExpr z e2)
+| z, .incr_at e1 e2 => .incr_at (zsmulBSExpr z e1) (zsmulBSExpr z e2)
+| z, .flipped e => if z > 0 then (.flipped (zsmulBSExpr z e)) else (zsmulBSExpr (-z) e)
+-- | z, .add e1 e2 => .add (zsmulBSExpr z e1) (zsmulBSExpr z e2)
+-- | n, .mul e
+
+
+def add: BSExpr → BSExpr → BSExpr
+| .zeros, e => e
+| e, .zeros => e
 |
 
 instance : Zero BSExpr where
-  zero := BSExpr.zeros
+  zero := .zeros
 
-instance : AddCommGroup CStream where
-  add := BSExpr.add
-  zero := BSExpr.zeros
-  neg := BSExpr.neg
-  nsmul := fun n s => if n % 2 = 0 then 0 else s
-  zsmul := fun z s => if z % 2 = 0 then 0 else s
+instance : AddCommGroup BSExpr where
+  add := .add
+  zero := .zeros
+  flipped := .flipped
+  nsmul := nsmulBSExpr
+  zsmul := zsmulBSExpr
 
 
 
@@ -77,7 +141,7 @@ instance : AddCommGroup CStream where
 -- instance : AddCommGroup CStream where
 --   add := (· + ·)
 --   zero := 0
---   neg := id
+--   flipped := id
 --   nsmul := fun n s => if n % 2 = 0 then 0 else s
 --   zsmul := fun z s => if z % 2 = 0 then 0 else s
 
